93d9e3358010e3431462952cc5d629e092b9734e
oldmonk699 <tushnikpatra674@gmail.com>
2026-02-15 05:08:30 +0530

    Fixed Pydantic schema error

--- schemas.py
+++ schemas.py
@@ -1,50 +1,58 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
+from typing import Optional
 from uuid import UUID
 
 
+# =========================
+# USER
+# =========================
+
 class UserCreate(BaseModel):
     username: str
-    email: EmailStr
+    email: str
     password: str
 
 
 class UserResponse(BaseModel):
     id: UUID
     username: str
-    email: EmailStr
+    email: str
 
     model_config = {
         "from_attributes": True
     }
-    
 
 
-
+# =========================
+# POST
+# =========================
 
 class PostCreate(BaseModel):
     title: str
     body: str
-    category: str
-    language: str
-    state: str
+    category: Optional[str] = None
+    language: Optional[str] = None
+    state: Optional[str] = None
 
 
 class PostResponse(BaseModel):
     id: UUID
     title: str
     body: str
-    category: str
-    language: str
-    state: str
+    category: Optional[str]
+    language: Optional[str]
+    state: Optional[str]
     user_id: UUID
 
     model_config = {
         "from_attributes": True
     }
 
-    from pydantic import BaseModel
+
+# =========================
+# RESET PASSWORD
+# =========================
 
 class ResetPasswordRequest(BaseModel):
     token: str
     new_password: str
-
